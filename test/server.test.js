f8ccae0a78c462e951d9ce556d7f761117607b5d
angel910 <angel910@me.com>
2024-10-16 20:55:46 -0400

    Before Submission

--- test/server.test.js
+++ test/server.test.js
@@ -5,9 +5,23 @@ const server = require('../app/server'); // Adjust the path as needed
 const should = chai.should();
 chai.use(chaiHttp);
 
-// TODO: Write tests for the server
 
-describe('Brands', () => {});
+
+describe('Brands', () => {
+  describe("/GET brands", () => {
+    it("it should GET a list of all the brands", (done) => {
+      chai
+        .request(server)
+        .get("/brands")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.length.should.be.eql(0);
+          done();
+        });
+    });
+  });
+});
 
 describe('Login', () => {});
 
